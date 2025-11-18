d11c1c4ff2a847403a9eabc70a7110dc8472f3a1
JonaC <jc2941211@gmail.com>
2025-11-18 02:39:54 -0600

    added menu images and styling to ExploreMenu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
+import { assets } from '../../assets/assets'
+
 
 const ExploreMenu = () => {
     return (
         <div className='explore-menu' id='explore-menu'>
             <h1>Explore our menu</h1>
-            <p className='explore-menu-text'>Choose from a diverse menu featuring a deletacble array of dishes. Our mission is to satisfy your cravings and elevate you dining experience, one delicious meal at at time</p>
-            <div className='explor-menu-list'>
+            <p className='explore-menu-text'>Choose from a diverse menu featuring a deletable array of dishes. Our mission is to satisfy your cravings and elevate you dining experience, one delicious meal at at time</p>
+            <div className='explore-menu-list'>
                 {menu_list.map((item, index) => {
                     return (
                         <div key={index} className='explore-menu-list-item'>
+                            {/* menu images */}
                             <img src={item.menu_image} alt="" />
                             <p>{item.menu_name}</p>
+
                         </div>
                     )
                 })}
             </div>
+            <hr />
         </div>
 
     )
